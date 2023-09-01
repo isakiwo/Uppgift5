895d80ec191f4f63e87d098c2de1c0e555caa3af
isakiwo <isak.ahlander.04@gmail.com>
2023-09-01 11:03:03 +0200

    Uppgift 5

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // JavaScript
 
 // Globala variabler
-
+let inputElem;
+let msgElem;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
 // Initiering av globala variabler samt koppling avfunktioner till knapparna.
 function init() {
-	
+	inputElem
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
